0f32ca6d592dab3663cd5f8193d93f28db918801
vaibhav-35 <77327065+vaibhav-35@users.noreply.github.com>
2021-10-24 18:20:40 +0530

    bug fix

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function rendertodo(val,currsize)
         templi.id=currsize;
         var delbtn = document.createElement('span');
         delbtn.classList.add('libtn');
-        delbtn.innerHTML+='<img class="updatebtn" src="update.png title="Update Item"> <img class="donebtn" src="done.jpg" height="25px" title="Task Done">  <img  class ="dltbtn" src="delete.jpg" height="25px" title="Delete Item">';
+        delbtn.innerHTML+='<img class="updatebtn" src="update.png" title="Update Item"> <img class="donebtn" src="done.jpg" height="25px" title="Task Done">  <img  class ="dltbtn" src="delete.jpg" height="25px" title="Delete Item">';
 
         
         let txt=document.createTextNode(val);
